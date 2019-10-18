78a1fb18b5fe87d63417e2d8c9f45a61fbf7a389
DhineshKSD <54356086+DhineshKSD@users.noreply.github.com>
2019-10-18 13:19:32 +0530

    Create Program.cs

---
+++ Task5/amsx/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           localhost.WebService1 obj = new localhost.WebService1();
+            int value1 = Convert.ToInt32(Console.ReadLine());
+
+            int value2 = Convert.ToInt32(Console.ReadLine());
+
+            string result = obj.Addition(value1, value2);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+
+        }
+    }
+}
